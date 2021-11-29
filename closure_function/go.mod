636477e9479a2b0f8c215ba4c26d5badff32acbd
disorn-inc <dirsorn1111@hotmail.com>
2021-11-29 21:01:53 +0700

    closure

---
+++ closure_function/go.mod
@@ -0,0 +1,3 @@
+module github/disorn-inc/go_fundamentals/closure_function
+
+go 1.16
---
+++ closure_function/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fn := newCounterFunc()
+	fmt.Println(fn())
+	fmt.Println(fn())
+	fmt.Println(fn())
+}
+
+func newCounterFunc() func() int {
+	var i int
+	return func() int {
+		i++
+		return i
+	}
+}
