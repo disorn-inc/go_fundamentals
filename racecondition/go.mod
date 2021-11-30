a6aa03f551cef0a5f50847e281834b88ade89c7e
disorn-inc <dirsorn1111@hotmail.com>
2021-11-30 21:44:15 +0700

    race condition

---
+++ racecondition/go.mod
@@ -0,0 +1,3 @@
+module github/disorn-inc/go_fundamentals/racecondition
+
+go 1.16
---
+++ racecondition/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var i int
+var mux sync.Mutex
+
+// in real life
+type thing struct {
+	i int
+	mux sync.Mutex
+}
+
+func main() {
+	go func()  {
+		for {
+			fmt.Println(read())
+		}
+	}()
+
+	for i := 0; i < 10; i++ {
+		write(i)
+	}
+}
+
+func write(n int) {
+	mux.Lock()
+	i = n
+	mux.Unlock()
+}
+
+func read() int {
+	mux.Lock()
+	defer mux.Unlock()
+	return i
+}
