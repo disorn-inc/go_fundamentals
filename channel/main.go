a515a9ec972fb0a831d4b7a08bfa65bac4d9044f
disorn-inc <dirsorn1111@hotmail.com>
2021-12-01 13:01:33 +0700

    select statement

--- channel/main.go
+++ channel/main.go
@@ -4,18 +4,29 @@ import "fmt"
 
 func main() {
 	ch := make(chan int)
-	go fibonacci(ch)
+	// 2 variable type use to send signal it should not use memory ex. struct{} and [0]func
+	qCh := make(chan struct{})
+
+	go fibonacci(ch, qCh)
 
 	for i := 0; i < 10; i++ {
 		fmt.Println(<-ch)
 	}
+	qCh <- struct{}{}
+	<-qCh
+	fmt.Println("end")
 }
 
-func fibonacci(ch chan int) {
+func fibonacci(ch chan int, qCh chan struct{}) {
 	a, b := 0, 1
 
 	for {
-		ch <- a
-		a, b = b, a+b
+		select {
+		case ch <- a:
+			a, b = b, a+b
+		case <- qCh:
+			qCh <- struct{}{}
+			return
+		}
 	}
 }
